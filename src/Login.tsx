1a9b7268d39826562be91a71ca75e360bf048987
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-10-12 20:11:04 -0400

    Sync commit

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './css/login.css';
+import logo_img from './assets/images/logo-full.png';
 
 function App() {
-	return <>
-		<h1>Hello, world!</h1>
-	</>
+    return <>
+        <img className="logo" src={logo_img} height="120" />
+        <input placeholder="Username" className="textbox" />
+    </>
 }
 
 export default App;
