885abaed7786063372f6f54ae74c7c21adcf6d2c
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-10-12 22:13:54 -0400

    Create try_login clientside function and error text

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import './css/login.css';
+import { invoke } from '@tauri-apps/api';
 import logo_img from './assets/images/logo-full.png';
+
+
+async function try_login() {
+    let userbox: HTMLInputElement = document.getElementById("login-namebox") as HTMLInputElement;
+    let passbox: HTMLInputElement = document.getElementById("login-passbox") as HTMLInputElement;
+    let username: string = userbox.textContent || "";
+    let password: string = userbox.textContent || "";
+
+    let success: boolean = await invoke("try_login", { username: username, password: password });
+    if (success) {
+        // Switch screens here
+    } else {
+        let errortext: HTMLParagraphElement = document.getElementById("errortext") as HTMLParagraphElement;
+        errortext.hidden = true;
+    }
+}
 
 function App() {
     return <>
-        <img className="logo" src={logo_img} height="120" />
-        <input placeholder="Username" className="textbox username" />
-        <input placeholder="Password" className="textbox password" />
+    <img className="logo" src={logo_img} height="120" />
+    <input placeholder="Username" className="textbox username" id="login-namebox" />
+    <input placeholder="Password" className="textbox password" id="login-passbox" type="password" />
+    <button onClick={try_login}>Login</button>
+    <p className="error-text" id="errortext" hidden={true}>Invalid username or password!</p>
     </>
 }
 
