952236e262c5098e5ac80ec31777ffb6cedf79e1
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-10-12 21:29:54 -0400

    Prevent right click within application

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import Login from './Login';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+document.addEventListener("contextmenu", ev => ev.preventDefault());
+
 root.render(
   <React.StrictMode>
     <Login />
