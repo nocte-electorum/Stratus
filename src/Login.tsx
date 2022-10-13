38d6d68b3ec132ed6a7780ab788f6a7a161bb429
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-10-12 20:25:09 -0400

    Add input fields to login form

--- src/Login.tsx
+++ src/Login.tsx
@@ -5,7 +5,8 @@ import logo_img from './assets/images/logo-full.png';
 function App() {
     return <>
         <img className="logo" src={logo_img} height="120" />
-        <input placeholder="Username" className="textbox" />
+        <input placeholder="Username" className="textbox username" />
+        <input placeholder="Password" className="textbox password" />
     </>
 }
 
