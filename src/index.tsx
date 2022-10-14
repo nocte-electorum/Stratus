5eaf34def6038780d2ebe56591d67a7fca981943
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-10-14 10:38:58 -0400

    Made default route Register.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import './css/index.css';
 import Login from './Login';
+import Register from './Register';
 
 
 const root = ReactDOM.createRoot(
@@ -14,6 +15,10 @@ document.addEventListener("contextmenu", ev => ev.preventDefault());
 export const router = createBrowserRouter([
     {
         path: "/",
+        element: Register()
+    },
+    {
+        path: "/login",
         element: Login()
     }
 ]);
