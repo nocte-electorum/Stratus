e28d03c39b4254d9fa3f88056f4a89751ea9f4ed
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-10-14 14:24:11 -0400

    Wrapped all the routing in an async function to allow for dynamic routing by backend

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import './css/index.css';
+import { invoke } from "@tauri-apps/api";
 import Login from './Login';
 import Register from './Register';
+import Dashboard from "./Dashboard";
 
 
 const root = ReactDOM.createRoot(
@@ -12,19 +14,29 @@ const root = ReactDOM.createRoot(
 
 document.addEventListener("contextmenu", ev => ev.preventDefault());
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: Register()
-    },
-    {
-        path: "/login",
-        element: Login()
-    }
-]);
-
-root.render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
-);
+// @ts-ignore
+export let router;
+(async function() : Promise<void> {
+    let is_registered: boolean = await invoke("is_registered");
+    router = createBrowserRouter([
+        {
+            path: "/",
+            element: (function() : React.ReactElement {
+                if (is_registered) {
+                    return Login();
+                } else {
+                    return Register();
+                }
+            })()
+        },
+        {
+            path: "/dash",
+            element: Dashboard()
+        }
+    ]);
+    root.render(
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    );
+})()
