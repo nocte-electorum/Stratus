76a769fe2eea813f823880a7d4fc3ad341f9c269
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-10-14 10:38:29 -0400

    Added dummy `is_logged_in()` backend function

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,5 +1,10 @@
 #[tauri::command]
 pub fn try_login(username: String, password: String) -> bool {
     println!("Tried login with {username} and {password}");
-    false
+    true
+}
+
+#[tauri::command]
+pub fn is_logged_in() -> bool {
+    true
 }
