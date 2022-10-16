5125189398097b6ace3592b38f40b8d8b5c6788c
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-10-16 02:07:34 -0400

    Update tauri backend command list to use new account functions

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,15 +1,16 @@
+use crate::functions::account;
+
 #[tauri::command]
 pub fn try_login(name: String, password: String) -> bool {
-    println!("Tried login with {name} and {password}");
-    true
+    account::try_login(&name, &password).is_ok()
 }
 
 #[tauri::command]
 pub fn is_registered() -> bool {
-    false
+    account::is_registered(&account::get_data_dir())
 }
 
 #[tauri::command]
 pub fn try_register(name: String, password: String) -> bool {
-    true
+    account::try_register(&name, &password).is_ok()
 }
