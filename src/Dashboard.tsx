89796155314ddf35f027a82991a7bf05c7368f4f
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-10-17 11:24:26 -0400

    Add starter dashboard file

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import styles from "./css/dashboard.module.css";
+import { invoke } from "@tauri-apps/api";
 
+
+let name: string = "";
+(async function() {
+    name = await invoke("get_name");
+    invoke("log", { s: `Found name ${name}` });
+})()
 
 function App() : React.ReactElement {
     return <>
-    <h1>Hello, world!</h1>
+    <h1 className={styles.title}>Welcome, {name}!</h1>
     </>
 }
 
